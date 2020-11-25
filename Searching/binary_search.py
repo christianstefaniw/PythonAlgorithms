88fb78f659066cca72c10c1df9127d1d33c130d7
ChristianStefaniw <christian.stefaniw@student.tdsb.on.ca>
2020-11-25 14:07:06 -0500

    Binary search

---
+++ Searching/binary_search.py
@@ -0,0 +1,20 @@
+def binary_search(arr, x):
+    start = 0
+    end = len(arr)-1
+
+    while end >= start:
+        mid = (end+start) // 2
+
+        if arr[mid] == x:
+            return mid
+
+        if arr[mid] > x:
+            end = mid - 1
+
+        if arr[mid] < x:
+            start = mid + 1
+
+
+    return -1
+
+print(binary_search([1,7,9,87,102,204,500,600,700], 102))
